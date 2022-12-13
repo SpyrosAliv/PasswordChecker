227364a018c1c19c5c6bc13c08d42c75a2cf6b22
SpyrosAliv <spyrosalivizatos8@gmail.com>
2022-12-13 22:55:15 +0200

    UpperCase Chcker
    Added a new rule that there should at least be 3 upper case characters.

--- PasswordChecker.py
+++ PasswordChecker.py
@@ -3,15 +3,26 @@ password = str(input("Enter password: "))
 def CheckPassword(password):
 
     numsCount = 0
-  
+    upperCases = 0
+
+    #Check for password's length
     if len(password) < 8:
         print("Password unsafe: less than 8 characters detected.")
 
+    #Check for integers inside the password
     for i in [*password]:
         if i in map(str, range(0, 10)):
             numsCount += 1
-            
+
     if numsCount < 4:
         print("Password unsafe: less than 4 integers detected.")
+
+    #TODO: Check for capitalisation in password
+    for i in [*password]:
+        if i.isupper():
+            upperCases += 1
+    if upperCases < 3:
+        print("Password unsafe: less than 3 capitalised characters detecetd")
+    
 
 CheckPassword(password)
