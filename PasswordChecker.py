91373123effa8c1e876192c8636c01cff440f524
SpyrosAliv <spyrosalivizatos8@gmail.com>
2022-12-13 23:12:14 +0200

    Safety measurement
    Added feature of safety measurement in percentage format.

--- PasswordChecker.py
+++ PasswordChecker.py
@@ -4,10 +4,15 @@ def CheckPassword(password):
 
     numsCount = 0
     upperCases = 0
+    unsafeCount = 0
+    safeCount = 0
 
     #Check for password's length
     if len(password) < 8:
         print("Password unsafe: less than 8 characters detected.")
+        unsafeCount += 1
+    else:
+        safeCount += 1
 
     #Check for integers inside the password
     for i in [*password]:
@@ -16,13 +21,21 @@ def CheckPassword(password):
 
     if numsCount < 4:
         print("Password unsafe: less than 4 integers detected.")
+        unsafeCount += 1
+    else:
+        safeCount += 1
 
-    #TODO: Check for capitalisation in password
+    #Check for capitalisation in password
     for i in [*password]:
         if i.isupper():
             upperCases += 1
     if upperCases < 3:
         print("Password unsafe: less than 3 capitalised characters detecetd")
+        unsafeCount += 1
+    else:
+        safeCount += 1
+    
+    print(f"Safety percentage: {int((safeCount / 3) * 100)}%")
     
 
 CheckPassword(password)
